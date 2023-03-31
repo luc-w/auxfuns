f0f629b93c59f6cc314f210afe04af37ed607b4a
luc-w <=>
2023-03-31 08:17:48 +0200

    minor bugfixes

--- R/plot_idiff.R
+++ R/plot_idiff.R
@@ -4,34 +4,34 @@
 #' mean variable with the associated error bars. The function can easily be
 #' extended with ggplot2 commands.
 #'
-#' @param data Either a data frame containing (only) dichotomous variables or the output of psych::describe()
+#' @param data Either a data frame or the output of psych::describe()
 #' @param guess Optional. Indicate the (theoretical) guessing probability.
-#' @param sort Optional. Keep variables in the same order as in the dataset (default), order them ascending order ("asc") or or
-#'             or order them in descending order ("desc").
+#' @param sort Optional. Decides whether the variables are plotted in their original order (default), in order of ascending ("asc")
+#'             or descending ("desc") mean values.
 #' @param group Optional. Character vector as long as the number of variables indicating their group. Item form different
 #'              groups will have different colors in the plot.
 #' @param obs Optional. Decides whether the number of observations is printed below the item label on the x-axis (Default: FALSE).
-#' @return A plot of the means with associated errors bars (2*SE)
+#' @return A plot of the variable means with associated errors bars (1*SE)
 #' @export
 
 
 
 plot_idiff <- function(data, guess = .25, sort = "orig", group = NULL, obs = FALSE){
-  
+
   data <- if(class(data)[2] == "describe") data else psych::describe(data)
-  
+
     data %>%
     as.data.frame() %>%
     tibble::rownames_to_column(., var="item") %>%
-    dplyr::mutate(group = group) %>% 
+    dplyr::mutate(group = group) %>%
     dplyr::mutate(item = if(obs){paste0(item, "\n", "(n=", n, ")")}
                          else {item},
-                  item = if(sort == "asc"){fct_reorder(item, mean)} 
-                         else if(sort == "desc") {fct_reorder(item, desc(mean))} 
+                  item = if(sort == "asc"){fct_reorder(item, mean)}
+                         else if(sort == "desc") {fct_reorder(item, desc(mean))}
                          else {item}) %>%
-    ggplot2::ggplot(., aes(x=item, y=mean, color = group)) +
+    ggplot2::ggplot(aes(x=item, y=mean, color = group)) +
                     geom_point() +
-                    coord_cartesian(ylim = c(0,1)) + 
+                    coord_cartesian(ylim = c(0,1)) +
                     scale_y_continuous(breaks=c(seq(0,1,0.1))) +
                     geom_hline(yintercept=guess, linetype = "dashed") +
                     geom_errorbar(aes(ymin=mean-1*se, ymax=mean+1*se, width = 0.3)) +
