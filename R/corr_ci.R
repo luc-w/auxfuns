bf9aa7035e42ed5f34242d408cde7d88c3821365
luc-w <=>
2023-10-06 10:35:16 +0200

    set default n.iter in corr_ci to 10000

--- R/corr_ci.R
+++ R/corr_ci.R
@@ -1,18 +1,19 @@
 #' Creates a correlation table with associated confidence intervals
 #'
-#' Creates a correlation matrix with associated bootstrapped 95% confidence invervals for easy export to Excel.
+#' Creates a correlation matrix with associated bootstrapped 95% confidence intervals for easy export to Excel.
 #' Based on psych::cor.ci().
 #'
 #' @param data Data frame.
+#' @param n.iter Number of iterations to bootstrap over.
 #' @param digits Number of digits to print.
 #' @param export Logical. If TRUE, the correlation table is exported as a csv file to the working directory. The default is FALSE.
 #' @param sep The field separator string to be used if export = TRUE.
 #' @return Correlation matrix with bootstrapped 95% confidence intervals.
 #' @import psych
 #' @export
-corr_ci <- function(data, digits = 2, export = FALSE, sep = ";"){
+corr_ci <- function(data, n.iter = 10000, digits = 2, export = FALSE, sep = ";"){
 
-  r <- psych::cor.ci(data, plot = F)
+  r <- psych::cor.ci(data, n.iter = n.iter, plot = F)
 
   r_est <- formatC(r$rho, digits = digits, format = "f")
 
